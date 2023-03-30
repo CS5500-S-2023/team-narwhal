a08b9ebde0c68b941788531b9c29be821147c21a
Matthew Fox <88064362+matthfox@users.noreply.github.com>
2023-03-30 07:29:58 +0000

    Comment out Modal code

--- src/main/java/edu/northeastern/cs5500/starterbot/command/AuthenticationSettingsCommand.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/command/AuthenticationSettingsCommand.java
@@ -15,10 +15,7 @@ import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEve
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
-import net.dv8tion.jda.api.interactions.components.ActionRow;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
-import net.dv8tion.jda.api.interactions.components.text.TextInput;
-import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
 import net.dv8tion.jda.api.utils.FileUpload;
 import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder;
 import net.dv8tion.jda.api.utils.messages.MessageCreateData;
@@ -61,24 +58,22 @@ public class AuthenticationSettingsCommand implements SlashCommandHandler, Butto
 
     @Override
     public void onButtonInteraction(@Nonnull ButtonInteractionEvent event) {
-        // Modal code example; not being used yet
-        TextInput testString =
-                TextInput.create("welcome", "Authenticate", TextInputStyle.SHORT)
-                        .setMinLength(1)
-                        .build();
-        TextInput message =
-                TextInput.create("welcome-message", "Message", TextInputStyle.PARAGRAPH)
-                        .setMinLength(1)
-                        .setPlaceholder("placeholder")
-                        .build();
+        // --- Modal code example ---
+        // TextInput testString =
+        //         TextInput.create("welcome", "Authenticate", TextInputStyle.SHORT)
+        //                 .setMinLength(1)
+        //                 .build();
+        // TextInput message =
+        //         TextInput.create("welcome-message", "Message", TextInputStyle.PARAGRAPH)
+        //                 .setMinLength(1)
+        //                 .setPlaceholder("placeholder")
+        //                 .build();
 
-        Modal modal =
-                Modal.create("welcome-modal", "Welcome")
-                        .addActionRows(ActionRow.of(testString), ActionRow.of(message))
-                        .build();
-
+        // Modal modal =
+        //         Modal.create("welcome-modal", "Welcome")
+        //                 .addActionRows(ActionRow.of(testString), ActionRow.of(message))
+        //                 .build();
         // event.replyModal(modal).queue();
-        // event.reply("You chose " + event.getButton().getLabel()).queue();
 
         switch (event.getButton().getId().split(":")[1]) {
             case "captcha":
