ff04ddacc549ebb94209c66a70011d0094118f38
lpy134340 <lpy134340@gmail.com>
2023-04-06 13:00:25 -0700

    update git branch

--- src/main/java/edu/northeastern/cs5500/starterbot/controller/UserPreferenceController.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/controller/UserPreferenceController.java
@@ -43,7 +43,6 @@ public class UserPreferenceController {
                 return currentUserPreference;
             }
         }
-
         UserPreference userPreference = new UserPreference();
         userPreference.setDiscordUserId(discordMemberId);
         userPreferenceRepository.add(userPreference);
